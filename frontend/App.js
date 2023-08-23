2daec61e900432338f6e15f18defd96375cd8034
pasan1227 <pasanratnayake@gmail.com>
2023-08-23 11:21:01 +0530

    Homepage Sign In function

--- frontend/App.js
+++ frontend/App.js
@@ -8,8 +8,14 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import { Card, Button, Row } from "react-bootstrap";
 
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+
+import Home from "./Components/Home";
+import NewPoll from "./Components/NewPoll";
+import PollingStation from "./Components/PollingStation";
+import { format } from "path";
 
 export default function App({ isSignedIn, contractId, wallet }) {
   const signInFunction = () => {
@@ -17,6 +23,35 @@ export default function App({ isSignedIn, contractId, wallet }) {
   };
   const signOutFunction = () => {
     wallet.signOut();
+  };
+
+  const displayHome = () => {
+    if (isSignedIn) {
+      return (
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/new-poll" element={<NewPoll />}></Route>
+          <Route path="/polling-station" element={<PollingStation />}></Route>
+        </Routes>
+      );
+    } else {
+      return (
+        <Container>
+          <Row className="justify-content-center d-flex">
+            <Card style={{ padding: "10px", marginTop: "10vh", width: "30vh" }}>
+              <Container>
+                <Row className="justify-content-center">
+                  Hey there! Please Sign In
+                </Row>
+                <Row className="justify-content-center d-flex">
+                  <Button onClick={signInFunction}>Login</Button>
+                </Row>
+              </Container>
+            </Card>
+          </Row>
+        </Container>
+      );
+    }
   };
 
   return (
@@ -28,7 +63,9 @@ export default function App({ isSignedIn, contractId, wallet }) {
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto"></Nav>
             <Nav>
-              <Nav.Link href="/new-poll">New Poll</Nav.Link>
+              <Nav.Link disabled={!isSignedIn} href="/new-poll">
+                New Poll
+              </Nav.Link>
               <Nav.Link onClick={isSignedIn ? signOutFunction : signInFunction}>
                 {isSignedIn ? wallet.accountId : "Login"}
               </Nav.Link>
@@ -36,6 +73,7 @@ export default function App({ isSignedIn, contractId, wallet }) {
           </Navbar.Collapse>
         </Container>
       </Navbar>
+      {displayHome()}
     </Router>
   );
 }
