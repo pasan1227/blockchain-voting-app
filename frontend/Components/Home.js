200402799fb37d71bd2cc2f605f66ef9d373f931
pasan1227 <pasanratnayake@gmail.com>
2023-08-23 12:02:18 +0530

    Poll List Table

--- frontend/Components/Home.js
+++ frontend/Components/Home.js
@@ -1,10 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Table, Container, Button, Row, Card } from "react-bootstrap";
 
 const Home = (props) => {
+  const [disabledButton, setDisabledButton] = useState(false);
+  const [promptList, setPromptList] = useState([]);
+
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <Container>
+      <Table style={{ margin: "5vh" }} striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>List of Polls</th>
+            <th>Go to Poll</th>
+          </tr>
+        </thead>
+        <tbody>
+          {promptList.map((item, index) => {
+            if (promptList.length) {
+              return (
+                <tr key={index}>
+                  <td>{index + 1}</td>
+                  <td>{item}</td>
+                  <td>
+                    <Button>Go to Poll</Button>
+                  </td>
+                </tr>
+              );
+            }
+          })}
+        </tbody>
+      </Table>
+      {promptList.length > 0 ? null : (
+        <Row className="justify-content-center d-flex">
+          <Card style={{ width: "20vw", height: "10vh" }}>
+            No Prompt to show
+          </Card>
+        </Row>
+      )}
+      <Row>
+        <Button
+          style={{
+            width: "20vh",
+            marginLeft: "10vh",
+          }}
+        >
+          Clear Polls
+        </Button>
+      </Row>
+    </Container>
   );
 };
 
