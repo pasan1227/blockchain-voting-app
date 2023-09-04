49f32aa6ed28824905582bcc33e4d5b620c7d4b3
pasan1227 <pasanratnayake@gmail.com>
2023-09-04 23:34:55 +0530

    Hero section component

--- frontend/pages/Home.js
+++ frontend/pages/Home.js
@@ -1,39 +1,11 @@
 import React from "react";
+import HeroSection from "../components/HeroSection";
 import FeatureSection from "../components/FeatureSection";
 
 export default function HeroImage() {
   return (
     <>
-      <section>
-        <header style={{ paddingLeft: 0 }}>
-          <div
-            className="p-5 text-center img-fluid bg-image"
-            style={{
-              backgroundImage:
-                "url('https://images.unsplash.com/photo-1451187580459-43490279c0fa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3572&q=80)",
-              height: 800,
-              backgroundSize: 1800,
-              backgroundRepeat: "no-repeat",
-            }}
-          >
-            <div className="d-flex justify-content-center align-items-center h-100">
-              <div className="text-white">
-                <h1 className="mb-3">Welcome to B-Vote</h1>
-                <h4 className="mb-3">
-                  A blockchain-based solution for secure and transparent voting.
-                </h4>
-                <a
-                  className="btn btn-outline-light btn-lg"
-                  href="#!"
-                  role="button"
-                >
-                  Get Started
-                </a>
-              </div>
-            </div>
-          </div>
-        </header>
-      </section>
+      <HeroSection />
       <FeatureSection />
     </>
   );
