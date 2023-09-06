13bfb8124e1f2a6479b2cfa7d18416d876032b7c
pasan1227 <pasanratnayake@gmail.com>
2023-09-06 09:25:04 +0530

    Updating aboute us page

--- frontend/pages/AboutUs.js
+++ frontend/pages/AboutUs.js
@@ -1,72 +1,69 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Image } from "react-bootstrap";
+
+import Footer from "../components/Footer";
 
 const AboutUs = () => {
   return (
-    <section id="about-us" className="py-5">
-      <Container>
-        <Row>
-          <Col>
-            <h2>About SecureVote</h2>
-            <p>
-              SecureVote is a pioneering organization dedicated to
-              revolutionizing the way elections are conducted. We are passionate
-              about ensuring that every citizen's voice is heard and every vote
-              is counted accurately and securely.
-            </p>
-            <p>
-              Our mission is to provide a secure and transparent voting platform
-              that empowers citizens to participate in elections with
-              confidence. We leverage blockchain technology to ensure the
-              integrity of the voting process.
-            </p>
-            <h3>Our Values</h3>
-            <ul>
-              <li>
-                <strong>Integrity:</strong> We are committed to upholding the
-                highest standards of integrity in all our operations.
-              </li>
-              <li>
-                <strong>Transparency:</strong> We believe in complete
-                transparency to build trust among our users and stakeholders.
-              </li>
-              <li>
-                <strong>Innovation:</strong> We continuously innovate to stay at
-                the forefront of secure voting technology.
-              </li>
-              <li>
-                <strong>Accessibility:</strong> We strive to make our voting
-                platform accessible to all, ensuring that no one is left behind.
-              </li>
-            </ul>
-            <h3>Our Team</h3>
-            <p>
-              SecureVote was founded by a group of dedicated individuals with
-              expertise in blockchain technology, cybersecurity, and elections.
-              Our team is passionate about creating a more secure and inclusive
-              democracy.
-            </p>
-            <p>
-              Meet our leadership team and the experts behind our voting system:
-            </p>
-            <ul>
-              <li>
-                <strong>John Doe</strong> - CEO and Co-Founder
-              </li>
-              <li>
-                <strong>Jane Smith</strong> - CTO and Co-Founder
-              </li>
-              <li>
-                <strong>Dr. Mark Johnson</strong> - Chief Security Officer
-              </li>
-              <li>
-                <strong>Susan Brown</strong> - Chief Operations Officer
-              </li>
-            </ul>
-          </Col>
-        </Row>
-      </Container>
-    </section>
+    <>
+      <section id="about-us" className="py-5 bg-light">
+        <Container>
+          <h2 className="text-center text-primary mb-4">About B-VOTE</h2>
+          <Row className="mb-4">
+            <Col md={6}>
+              <Image
+                src="../assets/images/about-us.jpeg"
+                alt="About Us"
+                fluid
+                className="about-us-image"
+              />
+            </Col>
+            <Col md={6}>
+              <h3>Our Mission</h3>
+              <p>
+                SecureVote is on a mission to transform the way elections are
+                conducted. We are committed to ensuring that every vote is
+                secure, transparent, and counts. Our goal is to provide a safe
+                and accessible platform for citizens to participate in the
+                democratic process.
+              </p>
+              <h3>Our Team</h3>
+              <p>
+                Our team consists of dedicated individuals with expertise in
+                blockchain technology, cybersecurity, and elections. Together,
+                we are working tirelessly to build a better future for
+                democracy.
+              </p>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <h3>Our Values</h3>
+              <ul className="values-list">
+                <li>
+                  <strong>Integrity:</strong> We uphold the highest standards of
+                  integrity in all our operations.
+                </li>
+                <li>
+                  <strong>Transparency:</strong> We believe in complete
+                  transparency to build trust among our users and stakeholders.
+                </li>
+                <li>
+                  <strong>Innovation:</strong> We continuously innovate to stay
+                  at the forefront of secure voting technology.
+                </li>
+                <li>
+                  <strong>Accessibility:</strong> We strive to make our voting
+                  platform accessible to all, ensuring that no one is left
+                  behind.
+                </li>
+              </ul>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+      <Footer />
+    </>
   );
 };
 
