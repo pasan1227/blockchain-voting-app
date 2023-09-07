54eaeefc365fdca180bc4e1e1ae3b63986bb7c2e
pasan1227 <pasanratnayake@gmail.com>
2023-09-07 11:09:57 +0530

    Adding CTA section

---
+++ frontend/components/CallToActionSection.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
+
+const CallToActionSection = () => {
+  return (
+    <section id="cta" className="py-5 bg- text-">
+      <Container>
+        <Row className="align-items-center">
+          <Col lg={6}>
+            <h2 className="mb-4">Ready to Get Started?</h2>
+            <p>
+              Join thousands of users who have already experienced secure and
+              transparent voting with B-Vote.
+            </p>
+          </Col>
+          <Col lg={6} className="text-lg-end">
+            <Button
+              variant="light"
+              size="lg"
+              href="/get-started"
+              className="btn-outline-dark"
+            >
+              Get Started
+            </Button>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default CallToActionSection;
